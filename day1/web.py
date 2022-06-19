eb82a5977148263dbca699b84aa530a22b13b551
aJay0422 <99143262+aJay0422@users.noreply.github.com>
2022-06-18 22:50:51 -0400

    update day01 (unfinished)

---
+++ day1/web.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates")   # 默认即为templates
+
+
+# 创建了网址 /show/info 和 函数index 的对应关系
+@app.route("/show/info")
+def index():
+    # return "<span style='color: red;'>中国</span>联通"
+    # Flask 内部自动打开文件，并读取内容，将内容返回给用户
+    # 默认：取当前项目目录的templates文件夹中找
+    return render_template("index.html")
+
+@app.route("/get/news")
+def get_news():
+    return render_template("get_news.html")
+
+if __name__ == "__main__":
+    app.run()
